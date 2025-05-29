98a85392635b77226e7553098dfe6d054db2aefd
David-Vijay <122029068+David-Vijay@users.noreply.github.com>
2025-05-29 19:21:50 +0530

    added cors headers defaults

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+from corsheaders.defaults import default_headers, default_methods
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -163,3 +163,5 @@ CORS_ALLOW_METHODS = [
     'DELETE',
     'OPTIONS',
 ]
+CORS_ALLOW_HEADERS = list(default_headers)  # <- import this
+CORS_ALLOW_METHODS = list(default_methods)
